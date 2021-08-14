8850987a1e5f58a932951b7b5298b96ae421fc1f
João Henrique Santana <joaoenrique.san@gmail.com>
2021-08-14 16:40:36 -0300

    Finish base

--- main.js
+++ main.js
@@ -1,30 +1,48 @@
-const grid_size = 10;
 const main_container = document.querySelector('main');
 
 let gridContainer = document.createElement('div');
 
 gridContainer.classList.add('grid-container');
-gridContainer.style.gridTemplateColumns = `repeat(${grid_size}, auto`;
-gridContainer.style.gridTemplateRows = `repeat(${grid_size}, auto`;;
+
 
 main_container.appendChild(gridContainer);
 
 let gridCells = []
 
+function createNewGrid(size){
+    if(!size){
+        let newGridSize = parseInt(prompt("What's the size of the grid ? (1-50)"));
+        console.log(newGridSize)
+        while(isNaN(newGridSize)||newGridSize > 50 || newGridSize < 1 ){
+            newGridSize = prompt("Please insert a number (1-50)");
+        }
+        gridSize = newGridSize;
+    }else{
+        gridSize = size;
+    }
+        
+    while (gridContainer.hasChildNodes()) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, auto)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, auto)`;
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell')
+        gridContainer.appendChild(cell);
+        gridCells.push(cell)
+    }
+    gridCells.forEach((cell) => {
+        cell.addEventListener('mouseenter', changeColor);
+    })
+
+}
 function changeColor(e) {
     let cell = e.target;
     cell.style.backgroundColor = 'black';
 }
-for (let i = 0; i < grid_size * grid_size; i++) {
-    let cell = document.createElement('div');
-    cell.classList.add('cell')
-    gridContainer.appendChild(cell);
-    gridCells.push(cell)
-}
-
-gridCells.forEach((cell) => {
-    cell.addEventListener('mouseenter', changeColor);
-})
 
 let button = document.querySelector('#clear-button');
 
@@ -32,5 +50,8 @@ function clearGrid(e){
     gridCells.forEach((cell)=>{
         cell.style.backgroundColor = "transparent"
     })
+    createNewGrid()
 }
 button.addEventListener('click', clearGrid)
+
+createNewGrid(10)
